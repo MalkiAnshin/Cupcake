0be8ccf53db8c2d923539db3e7eb5b8ec7da53ba
Malki Anshin <120270762+MalkiAnshin@users.noreply.github.com>
2024-05-26 21:12:40 +0300

    21:12 26\05\2024

--- src/ContactModal.jsx
+++ src/ContactModal.jsx
@@ -1,27 +1,39 @@
-import React from "react";
-import Modal from "react-modal";
-
-const ContactModal = ({ isOpen, onRequestClose }) => {
+export default function ContactModal({ isOpen, onCloseModal }) {
   return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      className="modal" 
-    >
-      <div className="bg-gradient-to-br from-orange-100 via-yellow-100 to-orange-100 p-6 rounded-lg mx-auto my-auto max-w-md">
-        <h4 className="text-white text-lg font-bold mb-4">:צרו איתנו קשר</h4>
-        <h2 className="text-white text-2xl font-bold">0533144444</h2>
-        <h2 className="text-white text-2xl font-bold">mamamam@gmail.com</h2>
-
-        <button
-          onClick={onRequestClose}
-          className="mt-4 px-4 py-2 bg-white text-gray-800 font-semibold rounded hover:bg-gray-200"
-        >
-          Close
-        </button>
-      </div>
-    </Modal>
+    <div>
+      {isOpen && (
+        <div className="fixed inset-0 flex items-center justify-center  bg-opacity-50">
+          <div className="bg-gradient-to-br from-orange-100 via-amber-100 to-orange-200 p-8 rounded-lg mx-auto my-auto max-w-lg text-center">
+            <h4 className="text-black text-lg font-bold mb-4">:צרו איתנו קשר</h4>
+            <div className="flex items-center mb-4">
+              <img
+                src="../assets/phone.png"
+                height={20}
+                width={20}
+                alt="icon phone"
+                className="mr-2"
+              />
+              <h2 className="text-black text-2xl font-bold">053-4134212</h2>
+            </div>
+            <div className="flex items-center mb-4">
+              <img
+                src="../assets/email.png"
+                height={20}
+                width={20}
+                alt="icon email"
+                className="mr-2"
+              />
+              <h2 className="text-black text-2xl font-bold">c0534134212@gmail.com</h2>
+            </div>
+            <button
+              onClick={onCloseModal}
+              className="mt-4 px-4 py-2 bg-white text-gray-800 font-semibold rounded hover:bg-gray-200"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   );
-};
-
-export default ContactModal;
+}
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,3 @@
-// Navbar.js
-
 import React, { useState } from "react";
 import ContactModal from "./ContactModal";
 
@@ -15,23 +13,28 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-full h-16 bg-transparent  top-0 z-50 backdrop-blur-sm px-10 items-center justify-between">
-      <br />
-      <br />
+    <div className="w-full h-16 bg-transparent top-0 z-50 backdrop-blur-sm px-10 items-center justify-between">
       <div className="flex flex-col items-center justify-center">
-        <button
-          onClick={openModal}
-          className="text-grey hover:text-yellow-700 transition duration-300"
-        >
-          <span className="mt-4 bg-transparent text-black font-bold py-2 px-3 rounded-full border-2 border-yellow-600 transition duration-300 hover:bg-amber-50 hover:bg-opacity-100 hover:bg-gradient-to-r from-transparent to-amber-100"
+        <br />
+        <br />
+        <br />
+  
+        {isModalOpen ? (
+          <ContactModal isOpen={isModalOpen} onCloseModal={closeModal} />
+        ) : (
+          <button
+            onClick={openModal}
+            className="text-gray hover:text-yellow-700 transition duration-300"
           >
-            צור קשר
-          </span>
-        </button>
+            <span className="mt-4 bg-transparent text-black font-bold py-2 px-3 rounded-full border-2 border-yellow-600 transition duration-300 hover:bg-amber-50 hover:bg-opacity-100 hover:bg-gradient-to-r from-transparent to-amber-100">
+              צור קשר
+            </span>
+          </button>
+        )}
       </div>
-      <ContactModal isOpen={isModalOpen} onRequestClose={closeModal} />
     </div>
   );
+    
 };
 
 export default Navbar;
